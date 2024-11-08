140c696df1ef100db13c205a52635688c2f20c13
V1OL3TF0X <mattex15243@gmail.com>
2024-11-08 21:19:36 +0100

    Add native fzf and update parsers

--- lua/setup/plugins/telescope.lua
+++ lua/setup/plugins/telescope.lua
@@ -1,23 +1,39 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    -- or                            , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim',
-        {
-            "nvim-telescope/telescope-live-grep-args.nvim",
-            -- This will not install any breaking changes.
-            -- For major updates, this must be adjusted manually.
-            version = "^1.0.0",
-        },
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.6',
+  -- or                            , branch = '0.1.x',
+  dependencies = { 'nvim-lua/plenary.nvim',
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
     },
-    config = function()
-        local telescope = require 'telescope'
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            telescope.extensions.live_grep_args.live_grep_args()
-        end)
-        telescope.load_extension("live_grep_args")
-    end,
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+  },
+  config = function()
+    local telescope = require 'telescope'
+    local builtin = require('telescope.builtin')
+    local lga_parser = require 'telescope-live-grep-args.prompt_parser'
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>ps', function()
+      vim.ui.input({ prompt = "Grep > " }, function(prompt)
+        if prompt == nil then
+          return;
+        end
+        local aa = lga_parser.parse(prompt)
+        if aa == nil then
+          return;
+        end
+        local s = table.remove(aa, 1)
+        builtin.grep_string({ search = s, additional_args = aa })
+      end)
+    end)
+    vim.keymap.set('n', '<leader>pS', function()
+      telescope.extensions.live_grep_args.live_grep_args()
+    end)
+    telescope.load_extension("live_grep_args")
+    telescope.load_extension("fzf")
+  end,
 };
