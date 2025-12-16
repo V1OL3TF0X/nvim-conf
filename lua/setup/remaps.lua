dda3e9c7258b170d47f0e1d28a8be65e46a9bf3d
V1OL3TF0X <mattex15243@gmail.com>
2025-12-16 11:49:22 +0100

    add file tree

--- lua/setup/remaps.lua
+++ lua/setup/remaps.lua
@@ -1,4 +1,3 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- navigation
 vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
@@ -33,6 +32,6 @@ vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 -- exit vertical insert mode with C-c
 vim.keymap.set('i', '<C-c>', '<Esc>')
 vim.keymap.set('n', '<leader>lc', function()
-    vim.cmd(string.format("e %s/lua/setup", vim.fn.stdpath 'config'))
+  vim.cmd(string.format("e %s/lua/setup", vim.fn.stdpath 'config'))
 end)
 vim.keymap.set('n', '<leader><leader>', function() vim.cmd 'so' end)
