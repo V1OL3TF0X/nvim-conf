4949d3bd5d8a6332bb6ffcc4abe594d8b45f1f69
V1OL3TF0X <mattex15243@gmail.com>
2025-12-22 12:24:03 +0100

    add vim surround

--- lua/setup/plugins/init.lua
+++ lua/setup/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
   { 'lukas-reineke/indent-blankline.nvim', main = 'ibl' },
+  { 'tpope/vim-surround',                  lazy = false },
   { 'tpope/vim-abolish',                   lazy = false },
   { 'mattn/emmet-vim',                     ft = { "html", "typescriptreact", "djangohtml", "javascriptreact", "php", "astro" } },
   { 'windwp/nvim-ts-autotag',              lazy = false },
